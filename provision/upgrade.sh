816f5f01697327a6ff04716d8dca91ad6edbf426
Luke Carrier <luke@carrier.im>
2020-08-05 20:59:53 +0100

    Force dpkg to behave noninteractively

--- provision/upgrade.sh
+++ provision/upgrade.sh
@@ -3,4 +3,6 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get dist-upgrade -y
+apt-get dist-upgrade -y \
+    -o Dpkg::Options::="--force-confdef" \
+    -o Dpkg::Options::="--force-confold"
